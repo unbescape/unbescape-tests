caa015eb97c0677ba48b00dbf3e1990950064674
Daniel Fernández <daniel@danfenz.dev>
2024-12-24 00:16:48 +0100

    Replaced StringEscapeUtils from commons-lang3 with commons-text

--- src/main/java/org/unbescape/xml/XmlBenchmark.java
+++ src/main/java/org/unbescape/xml/XmlBenchmark.java
@@ -23,7 +23,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Writer;
 
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 import org.owasp.esapi.codecs.XMLEntityCodec;
 
 /**
